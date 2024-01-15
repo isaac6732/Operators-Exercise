f185ee3e4095ab853f64bf6589bd64af8678da99
Isaac Oquendo <ioalvarez378@gmail.com>
2024-01-15 14:37:52 -0500

    Operator

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace OperatorExercise
+﻿using System.Reflection.Metadata;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            //Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+
+            Console.WriteLine("Enter the radius of you circle:");
+
+            var userInPut = Console.ReadLine();
+            
+            var radius = double.Parse(userInPut);
+            
+            Console.WriteLine(AreaOfCircle(radius));
+
         }
+
+        private static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+        
     }
+    
 }
+         
